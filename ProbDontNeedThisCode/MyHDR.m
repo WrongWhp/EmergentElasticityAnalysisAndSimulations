fbdc26c372f972b04457d3a51e95757758296fac
saocko <samocko@gmail.com>
2019-01-09 16:49:48 -0500

    d

--- ProbDontNeedThisCode/MyHDR.m
+++
@@ -1,9 +0,0 @@
-function [output] = MyHDR(input)
-output = input;
-output = output-min(output(:));
-output = output/max(output(:));
-output = .5 * (1+ tanh((output - .5) * 2));
-output = output-min(output(:));
-output = output/max(output(:));
-
-
